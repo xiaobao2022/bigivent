1e97fdef14ce334ea669c63848d3e331b1f722cf
小包 <wwwbde429@163.com>
2022-05-31 16:07:30 +0800

    完成了用户模块

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -14,7 +14,7 @@ $.ajaxPrefilter((option) => {
 
     // 每次发送请求回来校验token是否存在，或者是否过期
     option.complete = (res) => {
-        console.log(res);
+        // console.log(res);
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
             //  强制清空 token
